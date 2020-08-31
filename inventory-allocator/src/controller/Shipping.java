f24e976a5bb58266d26472ff9198eb7d0261ed15
Justin Palmer <jpalms2021@gmail.com>
2020-08-31 14:05:32 -0400

    Added comments

--- inventory-allocator/src/controller/Shipping.java
+++ inventory-allocator/src/controller/Shipping.java
@@ -13,25 +13,39 @@ public class Shipping {
         this.shipping = new ArrayList<>();
     }
 
-    public void update(String wharehouse, HashMap<String, Integer> product){
+    /**
+     * Description: Adds a new item to be shipped to the current shipping order
+     *
+     * @param warehouse - warehouse to that the item is coming from
+     * @param product - Item to be shipped
+     */
+    public void update(String warehouse, HashMap<String, Integer> product){
         boolean exists = false;
         int index = 0;
 
+        // Determines if the warehouse is already being shipped from
         for (int i = 0; i < shipping.size(); i++){
-            if(shipping.get(i).getName().equals(wharehouse)){
+            if(shipping.get(i).getName().equals(warehouse)){
                 exists = true;
                 index = i;
                 break;
             }
         }
 
+        // if true then update items being shipped to that warehouse
+        // otherwise add that warehouse and item to the shipping order
         if(exists){
             shipping.get(index).updateInv(product);
         } else{
-            shipping.add(new Warehouse(wharehouse, new Inventory(product)));
+            shipping.add(new Warehouse(warehouse, new Inventory(product)));
         }
     }
 
+    /**
+     * Description: String representation of items to be shipped
+     *
+     * @return - String
+     */
     @Override
     public String toString() {
         return this.shipping.toString();
