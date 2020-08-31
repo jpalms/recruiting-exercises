79bc6e2e346eef27e9001a14dc09bb095d452e17
Justin Palmer <jpalms2021@gmail.com>
2020-08-31 14:00:56 -0400

    Added comments

--- inventory-allocator/src/Allocator.java
+++ inventory-allocator/src/Allocator.java
@@ -8,26 +8,37 @@ import java.util.Scanner;
 
 public class Allocator {
 
+    /**
+     * Description: Takes in list of items to ship and warehouse information and outputs the Shipping order
+     *
+     * @param args - to lines of input
+     */
     public static void main(String []args){
+        // read in input
         Scanner input = new Scanner(System.in);
 
         String map = input.nextLine();
         String storage = input.nextLine();
 
-        map = map.substring(2);
+        // start input at first item
+        map = map.substring(map.indexOf(' ') + 1);
 
         HashMap<String, Integer> find = parseInventory(map);
 
+        // parse second input into a list of wharehouses
         ArrayList<Warehouse> warehouse = new ArrayList<>();
+        // loop while there is a unparsed storage
         while(storage.contains("name:")) {
             storage = storage.substring(storage.indexOf("name:"));
             warehouse.add(buildWharehouse(storage));
 
+            // makes read name unrecongized so the next on can be read
             storage = storage.substring(storage.indexOf("name:") + 1);
         }
 
         Shipping shipping = new Shipping();
 
+        // used to determine if an order is possible
         boolean exists = false;
         for (String product: find.keySet()) {
             exists = false;
@@ -36,6 +47,7 @@ public class Allocator {
 
             int sum = 0;
 
+            // determine if the item exists in an warehouse
             for (int i = 0; i < warehouse.size(); i++){
                 Warehouse w = warehouse.get(i);
                 if(w.hasProduct(product)){
@@ -43,6 +55,7 @@ public class Allocator {
 
                     matches.add(i);
 
+                    // gets first exact match, which should be the cheapest
                     if(w.exactMatch(product, find.get(product))){
                         exact = w;
                         break;
@@ -51,19 +64,24 @@ public class Allocator {
                 }
             }
 
+            // end if a item can't be found
             if(!exists){
                 break;
             }
             HashMap<String, Integer> tmp = new HashMap<>();
             tmp.put(product, find.get(product));
 
+            // if an exact match has been found
+            // otherwise look for a multiple warehouse order
             if(exact != null){
+                // adds match to shipping order and updates warehouse storage
                 shipping.update(exact.getName(), tmp);
                 int index = matches.get(matches.size()-1);
                 warehouse.set(index, warehouse.get(index).shipped(product, 0));
             } else{
                 tmp = new HashMap<>();
 
+                // determines if a multiple warehouse order is possible
                 if(sum < find.get(product)){
                     exists = false;
                     break;
@@ -79,11 +97,13 @@ public class Allocator {
                     int stored = w.getAmount(product);
 
                     if(stored > quota){
+                        // warehouse still has some items left
                         tmp.put(product, quota);
                         shipping.update(w.getName(), tmp);
                         warehouse.set(index, w.shipped(product, stored - quota));
                         break;
                     } else{
+                        // empty the warehouse of item
                         quota -= stored;
                         tmp.put(product, stored);
                         shipping.update(w.getName(), tmp);
@@ -102,7 +122,14 @@ public class Allocator {
         }
     }
 
-    public static Warehouse buildWharehouse(String storage){
+    /**
+     * Pre-con: A warehouse must have a least 1 item in Inventory
+     * Description: Parses a substring of input to build a single warehouse
+     *
+     * @param storage - substring to parse
+     * @return - Warehouse object with its inventory contents
+     */
+    private static Warehouse buildWharehouse(String storage){
         int index = storage.indexOf(":");
         String name = storage.substring(index + 2, storage.indexOf(","));
 
@@ -113,6 +140,13 @@ public class Allocator {
         return new Warehouse(name, new Inventory(items));
     }
 
+    /**
+     * Pre-con: At least one item
+     * Description: Parses a substring of input to build a Hashmap of the Inventory
+     *
+     * @param storage - substring to parse
+     * @return - Hashmap of items in storage
+     */
     private static HashMap<String, Integer> parseInventory(String storage){
 
         HashMap<String, Integer> items = new HashMap<String, Integer>();
